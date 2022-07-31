7a650262e818d5b6a137d7c9f32c5cd9dc1fe187
kulmboin <dltjrwn2000@gmail.com>
2022-07-31 23:19:57 +0900

    test

---
+++ blog/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+function Header() {
+  return <header>
+  <h1 className='title'>Blog</h1>
+</header>
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
